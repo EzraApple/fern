c6e593242435ce265d31372356101e0b40d59c2a
Ezra Apple <ezraapple.engineer@gmail.com>
2026-02-03 20:23:40 -0800

    eh

--- core/src/constants/timeouts.ts
+++
@@ -1,5 +0,0 @@
-/** Maximum duration for agent tasks in seconds (5 hours) */
-export const MAX_TASK_DURATION_SECONDS = 18000;
-
-/** Maximum duration for agent tasks in milliseconds */
-export const MAX_TASK_DURATION_MS = MAX_TASK_DURATION_SECONDS * 1000;
